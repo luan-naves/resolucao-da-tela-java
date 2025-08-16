62502c550adc9b603126581dc6771c348baef066
Luan Naves <luannf-@hotmail.com>
2025-08-16 03:43:30 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/resolucaotela/ResolucaoTela.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.resolucaotela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ResolucaoTela {
+
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("A resolução da sua tela é: " +d.width + " x " +d.height);
+    }
+}
